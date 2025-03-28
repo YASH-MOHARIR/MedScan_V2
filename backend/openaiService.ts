026bea6502d2d5b337661a7899e18b6503b0ed19
YASH-MOHARIR <68592074+YASH-MOHARIR@users.noreply.github.com>
2025-03-28 10:41:45 -0400

    Update openaiService.ts

--- backend/openaiService.ts
+++ backend/openaiService.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const OPENAI_API_KEY = process.env.OPEN_AI_API_KEY; // Store API key in .env
+const OPENAI_API_KEY =  import.meta.env.VITE_OPEN_AI_API_KEY;  
 const openaiEndpoint = "https://medscan-open-ai.openai.azure.com/openai/deployments/gpt-4o-mini/chat/completions?api-version=2024-10-21";
 
 export type chatMessageType =  { role: string, content: string };
